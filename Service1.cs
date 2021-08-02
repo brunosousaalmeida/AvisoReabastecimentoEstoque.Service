fc8838ef4561d983a09cdac67dff86fbe9e2b1c8
Bruno Sousa de Almeida Oliveira <bruno.almeida.developer@gmail.com>
2021-08-02 10:49:38 -0300

    Adicionar arquivos de projeto.

---
+++ Service1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvisoReabastecimentoEstoque.Service
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            timer1.Enabled = true;
+            timer1_Tick(null, null);
+        }
+
+        protected override void OnStop()
+        {
+            timer1.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
